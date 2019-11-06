3f1a94777f85bf223fee0b3a44286f79b62d64a1
Chris Rocco <chris.rocco7@gmail.com>
2019-11-06 12:22:03 -0600

    Added clarifying comment.

--- apod.sh
+++ apod.sh
@@ -1,5 +1,6 @@
 # ==========
 # Fetches NASA's Astronomy Picture of the Day (APOD) and sets it as the desktop background.
+# Get your NASA API key here: https://api.nasa.gov
 # ==========
 
 # Configuration
@@ -9,7 +10,7 @@ APOD_ENDPOINT=https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}
 
 # Fetch the APOD from NASA API.
 cd $IMAGE_DIR
-image_url=$(curl -s ${APOD_ENDPOINT} | jq -r .url)
+image_url=$(curl -s ${APOD_ENDPOINT} | jq -r .url)  # The API returns a JSON response. We use 'jq' to extract the 'url' attribute.
 file_name="APOD-$(date +%F).jpg"
 wget -q -O $file_name $image_url
 
